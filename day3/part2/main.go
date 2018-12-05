35aba4592f6bfd68848063ef2a8d25d173e07528
Eddie Sheffield <eddie@eddiesheffield.com>
2018-12-04 23:23:11 -0500

    Day 3 complete

--- day3/part2/main.go
+++ day3/part2/main.go
@@ -14,17 +14,19 @@ type Patch struct {
 	yOffset int
 	width   int
 	height  int
+	overlap bool
 }
 
 func processPatch(patch string) Patch {
 	var id, xOffset, yOffset, width, height int
 	fmt.Sscanf(patch, "#%d @ %d,%d: %dx%d", &id, &xOffset, &yOffset, &width, &height)
-	p := Patch{id, xOffset, yOffset, width, height}
+	p := Patch{id, xOffset, yOffset, width, height, false}
 	return p
 }
 
 func main() {
-	var cloth [1000][1000]int
+	var patches []Patch
+	var cloth [1000][1000][]int
 
 	path := os.Args[1]
 	lines, err := utils.ReadFile(path)
@@ -35,22 +37,33 @@ func main() {
 
 	multipatch := 0
 	for _, line := range lines {
-		patch := processPatch(line)
+		patches = append(patches, processPatch(line))
+	}
+
+	for p, patch := range patches {
 		for y := 0; y < patch.height; y++ {
 			for x := 0; x < patch.width; x++ {
 				row := patch.yOffset + y
 				col := patch.xOffset + x
-				cloth[row][col]++
+				cloth[row][col] = append(cloth[row][col], p)
 			}
 		}
 	}
 
 	for _, row := range cloth {
 		for _, col := range row {
-			if col > 1 {
+			if len(col) > 1 {
+				for _, p := range col {
+					patches[p].overlap = true
+				}
 				multipatch++
 			}
 		}
 	}
 	fmt.Println("Num shared patches: ", multipatch)
+	for _, patch := range patches {
+		if !patch.overlap {
+			fmt.Println("Non-overlaping patch: ", patch)
+		}
+	}
 }
