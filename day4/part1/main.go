94438a4fedd7ef6f5db275df29641c029e6ed2e5
Eddie Sheffield <eddie@eddiesheffield.com>
2018-12-10 00:23:57 -0500

    Fleshing out day 4

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -4,11 +4,54 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/esheffield/adventofcode2018/utils"
 )
+
+type Action int
+
+const (
+	NONE        Action = iota
+	BEGIN_SHIFT Action = iota
+	SLEEP       Action = iota
+	WAKE        Action = iota
+)
+
+type Entry struct {
+	t        time.Time
+	guardNum int
+	action   Action
+}
+
+func parseLine(source string) Entry {
+	line := strings.SplitAfterN(source, "]", 2)
+
+	re := regexp.MustCompile("#([\\d]+)")
+	action := NONE
+	guardNum := -1
+	if strings.HasPrefix(line[1], "wakes") {
+		action = WAKE
+	} else if strings.HasPrefix(line[1], "falls") {
+		action = SLEEP
+	} else {
+		action = BEGIN_SHIFT
+		guardNumParts := re.FindStringSubmatch(line[1])
+		if len(guardNumParts) == 2 {
+			guardNum, _ = strconv.Atoi(guardNumParts[1])
+		}
+	}
+	t, err := time.Parse("[2006-01-02 15:04]", line[0])
+
+	if err != nil {
+		panic(err)
+	}
+
+	return Entry{t, guardNum, action}
+}
 
 func main() {
 	args := os.Args
@@ -25,22 +68,16 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(lines[0])
-
-	source := "[1518-08-30 00:03] Guard #1307 begins shift"
-	line := strings.SplitAfterN(source, "]", 2)
-
-	fmt.Println(line)
-
-	re := regexp.MustCompile("#[\\d]+")
-
-	fmt.Printf("%q\n", re.FindString(line[1]))
-
-	t, err := time.Parse("[2006-01-02 15:04]", line[0])
-
-	if err != nil {
-		panic(err)
+	var entries []Entry
+	for _, line := range lines {
+		entries = append(entries, parseLine(line))
 	}
+	fmt.Println(entries)
 
-	fmt.Println(t)
+	sort.Slice(entries[:], func(i, j int) bool {
+		return entries[i].t.Before(entries[j].t)
+	})
+
+	fmt.Println(entries)
+
 }
